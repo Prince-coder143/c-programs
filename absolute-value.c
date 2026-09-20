5c173a2845a3b5a52f359993f876a483befdd264
Prince-coder143 <universitylifeprincekumar@gmail.com>
2026-09-20 09:29:25 +0530

    Add program to calculate absolute value

---
+++ absolute-value.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+// Calculate absolute value using abs()
+int number1;
+printf("Enter absolute value: \n");
+scanf("%d", &number1);
+printf("Absolute value: %d\n", abs(number1));
+return 0;
+}
